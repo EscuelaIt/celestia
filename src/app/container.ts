2436a5018ce4cc6c0a74d26499c7be56a9f7ef5b
cesalberca <cesar@cesalberca.com>
2025-08-21 18:11:57 +0200

    Add use case service to container

--- src/app/container.ts
+++ src/app/container.ts
@@ -1,7 +1,9 @@
 import { GetDestinationsQry } from '@/app/get-destinations.qry'
 import { CreateDestinationCmd } from '@/app/create-destination.cmd'
 import { CalculateTripCmd } from '@/app/calculate-trip.cmd'
+import { UseCaseService } from '@/app/use-case-service'
 
 export const getDestinationsQry = new GetDestinationsQry()
 export const createDestinationCmd = new CreateDestinationCmd()
 export const calculateTripCmd = new CalculateTripCmd()
+export const useCaseService = new UseCaseService()
