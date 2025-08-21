c567fbac73697cd380ed6ad2406db7b5e7d3912e
cesalberca <cesar@cesalberca.com>
2025-08-21 18:57:07 +0200

    Implement use case service

--- src/core/use-cases/use-case-service.ts
+++ src/core/use-cases/use-case-service.ts
@@ -1,7 +1,19 @@
 import type { UseCase } from '@/core/use-cases/use-case'
+import type { Middleware } from '@/core/use-cases/middlewares/middleware'
+import { UseCaseHandler } from '@/core/use-cases/use-case-handler'
+import { EmptyMiddleware } from '@/core/use-cases/middlewares/empty.middleware'
 
 export class UseCaseService {
+  constructor(private readonly middlewares: Middleware[]) {}
+
   async execute<In, Out>(useCase: UseCase<In, Out>, params?: In): Promise<Out> {
-    return useCase.execute(params)
+    let next = UseCaseHandler.create({ middleware: new EmptyMiddleware(), next: useCase })
+    for (let i = this.middlewares.length; i >= 0; i--) {
+      const currentMiddleware = this.middlewares[i]!
+      const previous = next
+      next = UseCaseHandler.create({ middleware: currentMiddleware, next: previous })
+    }
+
+    return next.execute(params) as Promise<Out>
   }
 }
