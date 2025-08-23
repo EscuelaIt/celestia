ab3a678416f3c724d77b34e6b8608f89669efa0e
cesalberca <cesar@cesalberca.com>
2025-08-23 13:41:35 +0200

    Create command

--- src/app/container.ts
+++ src/app/container.ts
@@ -1,7 +1,7 @@
-import { CreateDestinationUseCase } from '@/app/create-destination.use-case'
+import { CreateDestinationCmd } from '@/app/create-destination.cmd'
 import { GetDestinationsQry } from '@/app/get-destinations.qry'
 import { CalculateTripUseCase } from '@/app/calculate-trip.use-case'
 
-export const createDestinationUseCase = new CreateDestinationUseCase()
+export const createDestinationUseCase = new CreateDestinationCmd()
 export const getDestinationsUseCase = new GetDestinationsQry()
 export const calculateTripUseCase = new CalculateTripUseCase()
