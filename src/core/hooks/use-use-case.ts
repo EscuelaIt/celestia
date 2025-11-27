348fd683554f37b67e6031fee7cf56294ee4ff20
cesalberca <cesar@cesalberca.com>
2025-11-27 14:41:51 +0700

    Add is loading handling

--- src/core/hooks/use-use-case.ts
+++ src/core/hooks/use-use-case.ts
@@ -3,27 +3,45 @@ import { UseCaseService } from '@/core/use-cases/use-case-service'
 import type { UseCase, UseCaseParams, UseCaseReturn } from '@/core/use-cases/use-case'
 import type { WithInjectionToken } from '@/core/dependency-injection/with-injection-token'
 import type { AnyConstructor } from '@/core/types/any-constructor'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
-export interface UseCaseState<T extends UseCase> {
-  /** Function to execute the use case */
-  execute: (params?: UseCaseParams<T>) => Promise<UseCaseReturn<T>>
-}
+export type UseCaseState<T extends UseCase> = [
+  execute: (params?: UseCaseParams<T>) => Promise<UseCaseReturn<T>>,
+  { isLoading: boolean; data: UseCaseReturn<T> | undefined },
+]
 
-export function useUseCase<T extends UseCase>(useCaseClass: WithInjectionToken<AnyConstructor<T>>): UseCaseState<T> {
+export function useUseCase<T extends UseCase>(
+  useCaseClass: WithInjectionToken<AnyConstructor<T>>,
+  options?: { immediate?: boolean; defaultOptions?: UseCaseParams<T> },
+): UseCaseState<T> {
+  const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState<UseCaseReturn<T> | undefined>(undefined)
   const container = CelestiaContainer.getInstance()
+
+  useEffect(() => {
+    async function executeImmediately() {
+      const result = await execute(options?.defaultOptions)
+      setData(result)
+    }
+
+    if (options?.immediate) {
+      executeImmediately()
+    }
+  }, [])
 
   const execute = useCallback(
     async (params?: UseCaseParams<T>): Promise<UseCaseReturn<T>> => {
       const useCaseService = container.get(UseCaseService)
+      setIsLoading(true)
 
       const result = await useCaseService.execute(useCaseClass, params)
+
+      setIsLoading(false)
 
       return result
     },
     [useCaseClass],
   )
-  return {
-    execute,
-  }
+
+  return [execute, { isLoading, data }]
 }
--- src/features/destination/destination-list/delivery/destination-selector.tsx
+++ src/features/destination/destination-list/delivery/destination-selector.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Card, CardContent } from '@/core/components/ui/card'
 import { Button } from '@/core/components/ui/button'
@@ -14,20 +14,10 @@ import { useUseCase } from '@/core/hooks/use-use-case'
 
 export function DestinationSelector() {
   const router = useRouter()
-  const [destinations, setDestinations] = useState<Destination[]>([])
   const [selectedDestination, setSelectedDestination] = useState<Destination | null>(null)
   const [selectedShip, setSelectedShip] = useState<ShipType>('classic')
   const [showCreateForm, setShowCreateForm] = useState(false)
-  const { execute } = useUseCase(GetDestinationsQry)
-
-  useEffect(() => {
-    const fetchDestinations = async () => {
-      const destinations = await execute(GetDestinationsQry)
-      setDestinations(destinations)
-    }
-
-    fetchDestinations()
-  }, [])
+  const [, destinationState] = useUseCase(GetDestinationsQry, { immediate: true })
 
   const handlePlanTrip = () => {
     if (selectedDestination) {
@@ -39,8 +29,8 @@ export function DestinationSelector() {
   const handleDestinationCreated = async () => {
     setShowCreateForm(false)
     // Refresh destinations list
-    const destinations = await execute(GetDestinationsQry)
-    setDestinations(destinations)
+    // const destinations = await getDestinationsQry()
+    // setDestinations(destinations)
   }
 
   const handleCancelCreate = () => {
@@ -67,7 +57,7 @@ export function DestinationSelector() {
           </Button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {destinations.map(destination => (
+          {destinationState.data?.map(destination => (
             <Card
               key={destination.id}
               data-testid="destination-card"
